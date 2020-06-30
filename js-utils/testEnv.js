e5315c4acc7188d0248e4921fceaa0749aebea9c
Rob Secord <rob@pooltogether.com>
2020-06-30 18:46:12 -0400

    Updates to unit-tests

--- js-utils/testEnv.js
+++ js-utils/testEnv.js
@@ -6,12 +6,20 @@ const { expect } = require('chai')
 
 ethers.errors.setLogLevel('error')
 
+const getTestUsers = async () => {
+  // deployer is specified at index "0" in buidler.config.js
+  // vrfCoordinator is specified at index "1"
+  const [deployer, vrfCoordinator, user1, user2, stranger] = await buidler.ethers.getSigners()
+  return {deployer, vrfCoordinator, user1, user2, stranger}
+}
+
 module.exports = {
     buidler,
     deployments,
     ethers,
     expect,
 
+    getTestUsers,
     deployContract,
     deployMockContract,
 }
--- test/RNGBlockhash.test.js
+++ test/RNGBlockhash.test.js
@@ -1,6 +1,7 @@
 const {
   buidler,
   expect,
+  getTestUsers,
   deployMockContract,
 } = require('../js-utils/testEnv')
 
@@ -18,28 +19,22 @@ const debug = require('debug')('ptv3:RNG.test')
 
 
 describe('RNGBlockhash contract', function() {
-  let deployer
-  let user1
-  let user2
-
-  let rng, link
-
+  let users, rng, link
 
   beforeEach(async () => {
-    [deployer, user1, user2] = await buidler.ethers.getSigners()
-    const { vrfCoordinator } = await buidler.getNamedAccounts() // defined in buidler.config.js
+    users = await getTestUsers()
 
     debug('mocking LINK...')
-    link = await deployMockContract(deployer, LinkTokenInterface.abi, txOverrides())
+    link = await deployMockContract(users.deployer, LinkTokenInterface.abi, txOverrides())
 
     debug('deploying RNG...')
-    rng = await _getContract('RNGBlockhash', [vrfCoordinator, link.address], deployer)
+    rng = await _getContract('RNGBlockhashHarness', [users.vrfCoordinator._address, link.address])
   })
 
   describe('setKeyhash()', () => {
     it('should allow only the Owner to update the key-hash for VRF', async () => {
       // Non-Owner
-      await expect(rng.connect(user1).setKeyhash(VRF.keyHash.default))
+      await expect(rng.connect(users.stranger).setKeyhash(VRF.keyHash.default))
         .to.be.revertedWith('Ownable: caller is not the owner')
 
       // Owner
@@ -48,10 +43,22 @@ describe('RNGBlockhash contract', function() {
     })
   })
 
+  describe('setFee()', () => {
+    it('should allow only the Owner to update the fee for VRF', async () => {
+      // Non-Owner
+      await expect(rng.connect(users.stranger).setFee(VRF.fee.default))
+        .to.be.revertedWith('Ownable: caller is not the owner')
+
+      // Owner
+      await expect(rng.setFee(VRF.fee.default))
+        .to.not.be.revertedWith('Ownable: caller is not the owner')
+    })
+  })
+
   describe('setThreshold()', () => {
     it('should allow only the Owner to update the threshold for VRF', async () => {
       // Non-Owner
-      await expect(rng.connect(user1).setThreshold(VRF.threshold.default))
+      await expect(rng.connect(users.stranger).setThreshold(VRF.threshold.default))
         .to.be.revertedWith('Ownable: caller is not the owner')
 
       // Owner
@@ -66,7 +73,7 @@ describe('RNGBlockhash contract', function() {
       const smallAmount = toWei('10')
 
       // Non-Owner
-      await expect(rng.connect(user1).withdrawLink(smallAmount))
+      await expect(rng.connect(users.stranger).withdrawLink(smallAmount))
         .to.be.revertedWith('Ownable: caller is not the owner')
 
       // Owner; Insufficient balance
@@ -76,13 +83,13 @@ describe('RNGBlockhash contract', function() {
 
       // Owner; Sufficient balance; transfer fails
       await link.mock.balanceOf.withArgs(rng.address).returns(bigAmount)
-      await link.mock.transfer.withArgs(deployer._address, smallAmount).returns(false)
+      await link.mock.transfer.withArgs(users.deployer._address, smallAmount).returns(false)
       await expect(rng.withdrawLink(smallAmount))
         .to.be.revertedWith('RNGBlockhash/transfer-failed')
 
       // Owner; Sufficient balance
       await link.mock.balanceOf.withArgs(rng.address).returns(bigAmount)
-      await link.mock.transfer.withArgs(deployer._address, smallAmount).returns(true)
+      await link.mock.transfer.withArgs(users.deployer._address, smallAmount).returns(true)
       await expect(rng.withdrawLink(smallAmount))
         .to.not.be.revertedWith('RNGBlockhash/insuff-link')
         .to.not.be.revertedWith('RNGBlockhash/transfer-failed')
@@ -90,22 +97,89 @@ describe('RNGBlockhash contract', function() {
   })
 
   describe('requestRandomNumber()', () => {
-    it('should get a random number from the blockhash')
-    it('should get a random number from the VRF')
+    it('should get a random number from the blockhash', async () => {
+      const requestId = ethers.constants.One
+      const token = link.address
+      const budget = toWei('1')
+
+      await rng.setThreshold(budget.add(toWei('0.5'))) // Blockhash: budget < threshold
+      await link.mock.balanceOf.withArgs(rng.address).returns(toWei('100'))
+
+      await expect(rng.requestRandomNumber(token, budget))
+        .to.emit(rng, 'RandomNumberRequested')
+        .withArgs(requestId, users.deployer._address, token, budget)
+
+      // Confirm immediate completion
+      expect(await rng.isRequestComplete(requestId)).to.equal(true)
+    })
+
+    it('should get a random number from the VRF', async () => {
+      const requestId = ethers.constants.One
+      const token = link.address
+      const budget = toWei('1')
+
+      // Presets
+      await rng.setFee(VRF.fee.default)
+      await rng.setKeyhash(VRF.keyHash.default)
+      await rng.setThreshold(budget.sub(toWei('0.5'))) // VRF: budget >= threshold
+
+      // Mocks
+      await link.mock.balanceOf.withArgs(rng.address).returns(toWei('100'))
+
+      const blockhash = (await ethers.provider.getBlock()).hash
+      const seed = ethers.utils.solidityPack(['bytes32', 'uint256'], [VRF.keyHash.default, blockhash])
+      await link.mock.transferAndCall.withArgs(users.vrfCoordinator._address, VRF.fee.default, seed).returns(true)
+
+      // Test
+      await expect(rng.requestRandomNumber(token, budget))
+        .to.emit(rng, 'RandomNumberRequested')
+        .withArgs(requestId, users.deployer._address, token, budget)
+
+      // Confirm delayed completion
+      expect(await rng.isRequestComplete(requestId)).to.equal(false)
+    })
   })
 
   describe('isRequestComplete()', () => {
-    it('should check a request by ID and confirm if it is complete')
+    it('should check a request by ID and confirm if it is complete or not', async () => {
+      const requestId = ethers.constants.One
+      const RngRequestType = {INTERNAL: 0, CHAINLINK: 1} // Mock Enum
+
+      // Internal
+      await rng.setRequestType(requestId, RngRequestType.INTERNAL)
+      await rng.setRequestState(requestId, false)
+      expect(await rng.isRequestComplete(requestId)).to.equal(false)
+
+      await rng.setRequestState(requestId, true)
+      expect(await rng.isRequestComplete(requestId)).to.equal(true)
+
+      // Chainlink
+      await rng.setRequestType(requestId, RngRequestType.CHAINLINK)
+      await rng.setRequestState(requestId, false)
+      expect(await rng.isRequestComplete(requestId)).to.equal(false)
+
+      await rng.setRequestState(requestId, true)
+      expect(await rng.isRequestComplete(requestId)).to.equal(true)
+    })
   })
 
   describe('randomNumber()', () => {
-    it('should return a previous random number by request ID')
+    it('should return a previous random number by request ID', async () => {
+      const requestId = ethers.constants.One
+      await rng.setRandomNumber(requestId, 123)
+      expect(await rng.randomNumber(requestId)).to.equal(123)
+    })
   })
 
   describe('fulfillRandomness()', () => {
-    it('should allow only the VRF to fulfill VRF requests', async () => {
+    it('should disallow any account but the VRF to fulfill VRF requests', async () => {
+      // Not-VRF
       await expect(rng.fulfillRandomness(VRF.keyHash.default, toWei('12345')))
         .to.be.revertedWith('RNGBlockhash/invalid-vrf-coordinator')
+
+      // VRF
+      await expect(rng.connect(users.vrfCoordinator).fulfillRandomness(VRF.keyHash.default, toWei('12345')))
+        .to.not.be.revertedWith('RNGBlockhash/invalid-vrf-coordinator')
     })
   })
 })
