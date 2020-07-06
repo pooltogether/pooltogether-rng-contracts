6c614d2f9b552e1f72629ff317d7f2a017c7346b
Rob Secord <rob@pooltogether.com>
2020-07-06 19:53:06 -0400

    Update interface

--- contracts/RNGBlockhash.sol
+++ contracts/RNGBlockhash.sol
@@ -69,10 +69,6 @@ contract RNGBlockhash is RNGInterface, VRFConsumerBase, Ownable {
     require(LINK.transfer(msg.sender, amount), "RNGBlockhash/transfer-failed");
   }
 
-  function isLocked(uint256 blockNumber) external override view returns (bool) {
-    return requestCount == 0 || blockNumber == 0;
-  }
-
   /**
     *  WARNING NOTE: This can be called by anyone, potentially draining our $LINK
     *     TODO: Add caller protection mechanism? Governor? redirect payment?
--- contracts/RNGInterface.sol
+++ contracts/RNGInterface.sol
@@ -6,7 +6,6 @@ interface RNGInterface {
   event RandomNumberRequested(uint256 indexed id, address indexed sender, address token, uint256 budget);
   event RandomNumberCompleted(uint256 indexed id, uint256 randomNumber);
 
-  function isLocked(uint256 blockNumber) external view returns (bool);
   function requestRandomNumber(address token, uint256 budget) external returns (uint256);
   function isRequestComplete(uint256 id) external view returns (bool);
   function randomNumber(uint256 id) external view returns (uint256);
