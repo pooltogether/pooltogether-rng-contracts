f201e65d86721b840fc2e4474fd1bee279ebd628
Brendan Asselstine <mail.asselstine@gmail.com>
2021-05-07 12:34:21 -0700

    Deployed Chainlink VRF to Polygon.  Bumped version.

--- buidler.config.js
+++ buidler.config.js
@@ -89,7 +89,7 @@ module.exports = {
     },
     matic: {
       chainId: 137,
-      url: 'https://rpc-mainnet.maticvigil.com',
+      url: process.env.POLYGON_MAINNET_RPC_URL,
       accounts: {
         mnemonic: process.env.HDWALLET_MNEMONIC
       }
@@ -129,12 +129,14 @@ module.exports = {
       1: '0xf0d54349aDdcf704F77AE15b96510dEA15cb7952', // mainnet
       4: '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B', // rinkeby
       42: '0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9', // kovan
+      137: '0x3d2341ADb2D31f1c5530cDC622016af293177AE0', // matic / polygon
       80001: '0x8C7382F9D8f56b33781fE506E897a4F1e2d17255', // mumbai      
     },
     linkToken: {
       1: '0x514910771AF9Ca656af840dff83E8264EcF986CA', // mainnet
       4: '0x01BE23585060835E02B77ef475b0Cc51aA1e0709', // rinkeby
       42: '0xa36085F69e2889c224210F603D836748e7dC0088', // kovan
+      137: '0xb0897686c545045aFc77CF20eC7A532E3120E0F1', // matic / polygon
       80001: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB', // mumbai
     }
   }
--- vdf.config.js
+++ vdf.config.js
@@ -6,6 +6,7 @@ module.exports = {
     1       : '0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445', // mainnet
     4       : '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311', // rinkeby
     42      : '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4', // kovan
+    137     : '0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da', // matic / polygon
     80001   : '0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4'
   },
   fee: {
@@ -13,6 +14,7 @@ module.exports = {
     1       : ethers.utils.parseEther('2'), // 2 LINK
     4       : ethers.utils.parseEther('0.1'), // 0.1 LINK
     42      : ethers.utils.parseEther('0.1'), // 0.1 LINK
+    137     : ethers.utils.parseEther('0.0001'), // 0.0001 LINK
     80001   : ethers.utils.parseEther('0.0001') // 0.0001 LINK
   }
 }
