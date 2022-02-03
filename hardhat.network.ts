b90686a1d30372f4e8fc3b18f6231aa1a7ac98c6
Pierrick Turelier <pierrick@turelier.com>
2022-02-03 11:36:58 -0600

    fix(hardhat): fix network config

--- hardhat.network.ts
+++ hardhat.network.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 const infuraApiKey = process.env.INFURA_API_KEY;
 const mnemonic = process.env.HDWALLET_MNEMONIC;
+const polygonRPCUrl = process.env.POLYGON_MAINNET_RPC_URL;
 
 const networks: HardhatUserConfig['networks'] = {
   coverage: {
@@ -14,94 +15,112 @@ const networks: HardhatUserConfig['networks'] = {
     url: 'http://127.0.0.1:8545',
     allowUnlimitedContractSize: true,
   },
-  kovan: {
-    url: `https://kovan.infura.io/v3/${infuraApiKey}`,
-    accounts: {
-      mnemonic,
-      initialIndex: 0,
-      count: 3,
-    },
-  },
-  ropsten: {
-    url: `https://ropsten.infura.io/v3/${infuraApiKey}`,
-    accounts: {
-      mnemonic,
-      initialIndex: 0,
-      count: 3,
-    },
-  },
-  rinkeby: {
-    url: `https://rinkeby.infura.io/v3/${infuraApiKey}`,
-    accounts: {
-      mnemonic,
-      initialIndex: 0,
-      count: 3,
-    },
-  },
-  mainnet: {
-    url: `https://mainnet.infura.io/v3/${infuraApiKey}`,
-    accounts: {
-      mnemonic,
-      initialIndex: 0,
-      count: 3,
-    },
-  },
-  poaSokol: {
-    chainId: 77,
-    url: 'https://sokol.poa.network',
-    accounts: {
-      mnemonic,
-    },
-  },
-  xdai: {
-    chainId: 100,
-    url: 'https://xdai.poanetwork.dev',
-    accounts: {
-      mnemonic,
-    },
-  },
-  matic: {
-    chainId: 137,
-    url: process.env.POLYGON_MAINNET_RPC_URL,
-    accounts: {
-      mnemonic,
-    },
-  },
-  mumbai: {
+};
+
+if (mnemonic) {
+  networks.mumbai = {
     chainId: 80001,
     url: 'https://rpc-mumbai.maticvigil.com',
     accounts: {
       mnemonic,
     },
-  },
-  bsc: {
+  };
+
+  networks.bsc = {
     chainId: 56,
     url: 'https://bsc-dataseed.binance.org',
     accounts: {
       mnemonic,
     },
-  },
-  bscTestnet: {
+  };
+
+  networks.bscTestnet = {
     chainId: 97,
     url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
     accounts: {
       mnemonic,
     },
-  },
-  celo: {
+  };
+
+  networks.celo = {
     chainId: 42220,
     url: 'https://forno.celo.org',
     accounts: {
       mnemonic,
     },
-  },
-  celoTestnet: {
+  };
+
+  networks.celoTestnet = {
     chainId: 44787,
     url: 'https://alfajores-forno.celo-testnet.org',
     accounts: {
       mnemonic,
     },
-  },
-};
+  };
+
+  networks.poaSokol = {
+    chainId: 77,
+    url: 'https://sokol.poa.network',
+    accounts: {
+      mnemonic,
+    },
+  };
+
+  networks.xdai = {
+    chainId: 100,
+    url: 'https://xdai.poanetwork.dev',
+    accounts: {
+      mnemonic,
+    },
+  };
+
+  if (infuraApiKey) {
+    networks.kovan = {
+      url: `https://kovan.infura.io/v3/${infuraApiKey}`,
+      accounts: {
+        mnemonic,
+        initialIndex: 0,
+        count: 3,
+      },
+    };
+
+    networks.ropsten = {
+      url: `https://ropsten.infura.io/v3/${infuraApiKey}`,
+      accounts: {
+        mnemonic,
+        initialIndex: 0,
+        count: 3,
+      },
+    };
+
+    networks.rinkeby = {
+      url: `https://rinkeby.infura.io/v3/${infuraApiKey}`,
+      accounts: {
+        mnemonic,
+        initialIndex: 0,
+        count: 3,
+      },
+    };
+
+    networks.mainnet = {
+      url: `https://mainnet.infura.io/v3/${infuraApiKey}`,
+      accounts: {
+        mnemonic,
+        initialIndex: 0,
+        count: 3,
+      },
+    };
+  }
+
+  if (polygonRPCUrl) {
+    networks.matic = {
+      chainId: 137,
+      url: process.env.POLYGON_MAINNET_RPC_URL,
+      accounts: {
+        mnemonic,
+      },
+    };
+  }
+}
 
 export default networks;
